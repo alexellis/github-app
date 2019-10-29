28dbb73e80f6a65175dff62a6e8e1181b9f60b35
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-10-29 19:32:17 +0000

    Exchange for token
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- github-app/handler.go
+++ github-app/handler.go
@@ -1,19 +1,35 @@
 package function
 
 import (
+	"bytes"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
 func Handle(w http.ResponseWriter, r *http.Request) {
-
 	if r.Body != nil {
 		defer r.Body.Close()
 	}
 
-	body, _ := ioutil.ReadFile("function/index.html")
+	if r.URL.Path == "/callback" {
+		code := r.URL.Query().Get("code")
+		reader := bytes.NewBufferString(code)
+		res, err := http.Post(fmt.Sprintf("https://api.github.com/app-manifests/%s/conversions", code), "application/vnd.github.fury-preview+json", reader)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if res.Body != nil {
+			defer res.Body.Close()
+			result, _ := ioutil.ReadAll(res.Body)
+			w.Write(result)
+		}
+	}
+
+	res, _ := ioutil.ReadFile("function/index.html")
 
 	w.WriteHeader(http.StatusOK)
-	w.Write(body)
+	w.Write(res)
 
 }
